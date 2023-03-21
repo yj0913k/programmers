52a1e068c8d05f142a13751d19c48d8d23fa44a8
youngjun <yj0913k@naver.com>
2023-03-21 21:40:51 +0900

    Q42578.java

--- src/main/java/com/rank2/Q42578.java
+++ src/main/java/com/rank2/Q42578.java
@@ -1,4 +1,23 @@
 package com.rank2;
 
+import java.util.*;
+
 public class Q42578 {
+    public static void main(String[] args) {
+        String[][] clothes = 	{{"crow_mask", "face"}, {"blue_sunglasses", "face"}, {"smoky_makeup", "face"}};
+
+        HashMap<String, Integer> hash= new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+            if(hash.containsKey(clothes[i][1])) hash.put(clothes[i][1], hash.get(clothes[i][1]) + 1);
+            else hash.put(clothes[i][1], 1);
+        }
+
+        int answer = 1;
+        for (Integer i : hash.values()) {
+            answer*=i;
+        }
+        System.out.println(answer);
+
+    }
 }
